89662fcb15eb3dbe34ea92ef7ba66514f24186a9
Matt Thompson <matt@thompsn.com>
2016-09-14 00:29:34 -0500

    update modules to conform with pep8 style standards

--- GA01/weather/current.py
+++ GA01/weather/current.py
@@ -1,6 +1,7 @@
 import requests
 
-# Weather data format defaults to 'us', but can be optionally set as 'si'
+# Weather data format defaults to 'us',
+# but can be optionally set as 'si'
 
 _openWeatherApiKey = '84ae7c721ed64a70da9c1ddb07ba7416'
 
@@ -8,18 +9,26 @@ _openWeatherApiKey = '84ae7c721ed64a70da9c1ddb07ba7416'
 _defaultLat = '42.0307810'
 _defaultLng = '-93.6319130'
 
-def _sendRequest(lat,lng, format):
+
+def _sendRequest(lat, lng, format):
     if format == 'us':
         format = 'imperial'
     elif format == 'si':
         format = 'metric'
-    r = requests.get('http://api.openweathermap.org/data/2.5/weather?appid='+_openWeatherApiKey+'&lat='+lat+'&lon='+lng+'&units='+format)
-    data=r.json()
+    r = requests.get(
+        'http://api.openweathermap.org/data/2.5/weather?appid=' +
+        _openWeatherApiKey +
+        '&lat=' + lat +
+        '&lon=' + lng +
+        '&units='+format
+        )
+    data = r.json()
     return data
+
 
 # returns all relevant data
 def getSnapshot(format='us'):
-    data = _sendRequest(_defaultLat,_defaultLng,format)
+    data = _sendRequest(_defaultLat, _defaultLng, format)
     response = {
         'temp': data['main']['temp'],
         'windSpeed': data['wind']['speed'],
@@ -29,27 +38,32 @@ def getSnapshot(format='us'):
     }
     return response
 
+
 # returns temp
 def getTemp(format='us'):
-    data = _sendRequest(_defaultLat,_defaultLng,format)
+    data = _sendRequest(_defaultLat, _defaultLng, format)
     return data['main']['temp']
+
 
 # returns wind speed
 def getWindSpeed(format='us'):
-    data = _sendRequest(_defaultLat,_defaultLng,format)
+    data = _sendRequest(_defaultLat, _defaultLng, format)
     return data['wind']['speed']
+
 
 # returns wind direction
 def getWindDir(format='us'):
-    data = _sendRequest(_defaultLat,_defaultLng,format)
+    data = _sendRequest(_defaultLat, _defaultLng, format)
     return data['wind']['deg']
+
 
 # returns pressure
 def getPressure(format='us'):
-    data = _sendRequest(_defaultLat,_defaultLng,format)
+    data = _sendRequest(_defaultLat, _defaultLng, format)
     return data['main']['pressure']
+
 
 # returns humidity
 def getHumidity(format='us'):
-    data = _sendRequest(_defaultLat,_defaultLng,format)
+    data = _sendRequest(_defaultLat, _defaultLng, format)
     return data['main']['pressure']
--- GA01/weather/historical.py
+++ GA01/weather/historical.py
@@ -1,6 +1,7 @@
 import requests
 
-# Weather data format defaults to 'us', but can be optionally set as 'si' to use
+# Weather data format defaults to 'us', but
+# can be optionally set as 'si' to use
 # the International System of Units.
 
 _forecastApiKey = '5e4981d279ea465087d2eec8584e9dcd'
@@ -9,14 +10,23 @@ _forecastApiKey = '5e4981d279ea465087d2eec8584e9dcd'
 _defaultLat = '42.0307810'
 _defaultLng = '-93.6319130'
 
-def _sendRequest(time,lat,lng, format):
-    r = requests.get('https://api.forecast.io/forecast/'+_forecastApiKey+'/'+lat+','+lng+','+time+'?units='+format)
-    data=r.json()
+
+def _sendRequest(time, lat, lng, format):
+    r = requests.get(
+        'https://api.forecast.io/forecast/' +
+        _forecastApiKey +
+        '/' + lat +
+        ',' + lng +
+        ',' + time +
+        '?units='+format
+        )
+    data = r.json()
     return data
 
+
 # returns all relevant data for specefied time
-def getSnapshot(time,format='us'):
-    data = _sendRequest(time,_defaultLat,_defaultLng,format)
+def getSnapshot(time, format='us'):
+    data = _sendRequest(time, _defaultLat, _defaultLng, format)
     response = {
         'temp': data['currently']['temperature'],
         'windSpeed': data['currently']['windSpeed'],
@@ -26,27 +36,32 @@ def getSnapshot(time,format='us'):
     }
     return response
 
+
 # returns temp for specefied time
-def getTemp(time,format='us'):
-    data = _sendRequest(time,_defaultLat,_defaultLng,format)
+def getTemp(time, format='us'):
+    data = _sendRequest(time, _defaultLat, _defaultLng, format)
     return data['currently']['temperature']
 
+
 # returns wind speed for specefied time
-def getWindSpeed(time,format='us'):
-    data = _sendRequest(time,_defaultLat,_defaultLng,format)
+def getWindSpeed(time, format='us'):
+    data = _sendRequest(time, _defaultLat, _defaultLng, format)
     return data['currently']['windSpeed']
 
+
 # returns wind direction for specefied time
-def getWindDir(time,format='us'):
-    data = _sendRequest(time,_defaultLat,_defaultLng,format)
+def getWindDir(time, format='us'):
+    data = _sendRequest(time, _defaultLat, _defaultLng, format)
     return data['currently']['windBearing']
 
+
 # returns pressure for specefied time
-def getPressure(time,format='us'):
-    data = _sendRequest(time,_defaultLat,_defaultLng,format)
+def getPressure(time, format='us'):
+    data = _sendRequest(time, _defaultLat, _defaultLng, format)
     return data['currently']['pressure']
 
+
 # returns humidity for specefied time
-def getHumidity(time,format='us'):
-    data = _sendRequest(time,_defaultLat,_defaultLng,format)
+def getHumidity(time, format='us'):
+    data = _sendRequest(time, _defaultLat, _defaultLng, format)
     return data['currently']['humidity']
