606a1ccae09b9fdef6235312e67c95826585ebac
Keng Hu <keng@iastate.edu>
2016-09-21 15:51:02 -0500

    Current time
    Add current time command line to print out current time each time we run
    the test file.

--- GA01/test_suite/test_script.py
+++ GA01/test_suite/test_script.py
@@ -1,1 +1,9 @@
 #test_script
+import time
+# from GA01 import AmesWeather  (having issue to import)
+
+
+currentTime = time.strftime("%H:%M:%S")
+currentDate = time.strftime("%d/%m/%Y")
+print(currentTime)
+print(currentDate)
