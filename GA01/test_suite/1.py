a251796200eff1204b7e15bcf318d5b45f1d8167
Keng Hu <keng@iastate.edu>
2016-09-22 12:49:11 -0500

    updates for test_script.py

---
+++ GA01/test_suite/1.py
@@ -0,0 +1,4 @@
+import sys
+
+sys.path.insert(0, 'D:\\2016 Fall\\MIS 407\\Git Bash\\TEAM-ULTIMATE\\GA01')
+import AmesWeather
--- GA01/test_suite/test_script.py
+++ GA01/test_suite/test_script.py
@@ -1,9 +1,31 @@
 #test_script
+
 import time
-# from GA01 import AmesWeather  (having issue to import)
+import unittest
+import sys
+sys.path.insert(0, 'D:\\2016 Fall\\MIS 407\\Git Bash\\TEAM-ULTIMATE\\GA01')
+import AmesWeather as AW
 
+def timePrinter():
+    currentTime = time.strftime("%H:%M:%S")
+    currentDate = time.strftime("%d/%m/%Y")
+    print
+    print(currentTime)
+    print(currentDate)
+# turns the string into python's datetime format
+testDate = AW.stringToDate('2/12/2015:17:37')
+print(testDate)
 
-currentTime = time.strftime("%H:%M:%S")
-currentDate = time.strftime("%d/%m/%Y")
-print(currentTime)
-print(currentDate)
+# turns the datetime object into a UNIX timestamp
+testTimeStamp = AW.dateToTimeStamp(testDate)
+print(testTimeStamp)
+
+# turns the datetime object into a UNIX timestamp for 1 day before
+print(AW.dateOffset(testDate, 'D'))
+# turns the datetime object into a UNIX timestamp for 1 month before
+print(AW.dateOffset(testDate, 'M'))
+
+# gets temp from timestamp
+print(AW.historical.getTemp(str(testTimeStamp)))
+
+print(timePrinter())
