4c1df419aeac47e389a5ed5242ade19a48359869
lchol <lchol@iastate.edu>
2016-09-23 13:02:00 -0500

    Test Sctipt

--- GA01/test_suite/test_script.py
+++ GA01/test_suite/test_script.py
@@ -3,6 +3,7 @@
 import time
 import unittest
 import sys
+import os
 # use sys to find the location of GA1 folder to import the AmesWeather
 # use sys.path to find the location of this file
 # however, we need to get rid of test_script to arrive in GA1 folder
@@ -12,34 +13,10 @@ a = i[0].split("\\")
 b = ""
 # put the words in the array back on with '\' to get the location of GA1
 for i in range(len(a)-1):
-        b = b + a[i] + '\\'
+        b = b + a[i] + '\\' + '\\'
 
 b = b[:-1]
 # ask python to find path of the Group Assignment folder
-sys.path.insert(0, b)
-import AmesWeather as AW
 
-# print out the current time and current date
-def timePrinter():
-    currentTime = time.strftime("%H:%M:%S")
-    currentDate = time.strftime("%d/%m/%Y")
-    print
-    print(currentTime)
-    print(currentDate)
-
-# main test class is created below.
-# unittest is used to test the AmesWeather.py
-class testHistorical(unittest.TestCase):
-
-
-    def test_temp_true(self):
-        testDate = AW.stringToDate('2/12/2015:17:37')
-        testTimeStamp = AW.dateToTimeStamp(testDate)
-        print(testTimeStamp)
-        timePrinter()
-        self.assertTrue(self)
-
-
-
-if __name__ == '__main__':
-    unittest.main()
+os.chdir(b)
+os.system('python AmesWeather.py P T WS WD')
