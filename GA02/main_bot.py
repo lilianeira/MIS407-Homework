5663b53033cfa51e33a93cd1db8680d3c0b539d9
Keng Hu <keng@iastate.edu>
2016-10-21 14:57:32 -0500

    bot is able to send the messages. updates on printing out the channels and information of channels. Make sure to use our team API TOKEN to run it!

---
+++ GA02/main_bot.py
@@ -0,0 +1,47 @@
+# There is a couple of thing that you need to do to run this script.
+# slackclient needs to be installed by pip install slackclient
+import time
+import os
+from slackclient import SlackClient
+
+token = "xoxb-91109580036-FRjGcFWSrRUTJHLJ76sLAO4i"
+sc = SlackClient(token)
+token = os.environ.get(token, None)
+def list_channels():
+    channels_call = sc.api_call("channels.list")
+    if channels_call.get('ok'):
+        return channels_call['channels']
+    return None
+def channels_print():
+    channels = list_channels()
+    if channels:
+        print("Channels: ")
+        for c in channels:
+            print(c['name'] + " (" + c['id'] + ")")
+    else:
+        print("Unable to authenticate.")
+
+def channel_info(channel_id):
+    channel_info = sc.api_call("channels.info", channel=channel_id)
+    if channel_info:
+        return channel_info['channel']
+    return None
+
+def send_message(channel_id, message):
+    sc.api_call("chat.postMessage", channel = channel_id, text = message, username = "pybot", icon_emoji = 'robot_face')
+
+if __name__ == '__main__':
+    channels = list_channels()
+    if channels:
+        print("Channels: ")
+        for c in channels:
+            print(c['name'] + " (" + c['id'] + ")")
+            detailed_info = channel_info(c['id'])
+            '''
+            if detailed_info:
+                print(detailed_info())
+            '''
+            if c['name'] == 'general':
+                send_message(c['id'], "Hello" + c['name'] + "! It worked!")
+    else:
+        print("Unable to authenticate.")
