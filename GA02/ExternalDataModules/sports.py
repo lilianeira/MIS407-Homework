d19c5d8760eb422212c61655173bfc574f31d708
lilianeira <liragena@iastate.edu>
2016-10-30 16:24:23 -0500

    code clean up

--- GA02/ExternalDataModules/sports.py
+++ GA02/ExternalDataModules/sports.py
@@ -1,12 +1,6 @@
 '''Pull sports data from Cyclones.com'''
-import lxml
-import lxml.html
-import urllib
 import requests
-import feedparser
 import re
-from BeautifulSoup import BeautifulSoup
-
 import feedparser
 import time
 from subprocess import check_output
@@ -18,7 +12,6 @@ print(uptime.strip())
 print('-------------------------------------------------------------')
 print("\n")
 
-url = "http://www.cyclones.com/schedule.aspx?path=mbball"
 d = feedparser.parse('http://cyclones.com/rss.aspx?path=football')
 
 
