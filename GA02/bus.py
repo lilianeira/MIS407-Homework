44d15dd2d707ab0f406fd55c85d1c1cb24e6a4f2
wvoong <wvoong@iastate.edu>
2016-10-31 09:59:59 -0500

    Added XML parser

---
+++ GA02/bus.py
@@ -0,0 +1,17 @@
+import requests
+import xml.etree.ElementTree as ET
+
+agency = 'cyride'
+
+_nextBusXMLAPIKey =
+
+def getRouteTimes(stop):
+    r = requests.get(
+        'http://webservices.nextbus.com/service/publicXMLFeed?'
+        'command=predictions' +
+        '&a=' + agency +
+        '&stopId=' + stop +
+        '&useShortTitles=true'
+        )
+    data = r.json()
+    return data
