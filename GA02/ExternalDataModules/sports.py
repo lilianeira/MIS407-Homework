cdeaea2e299c645c18a6da0f73b9e35c75a90c99
Matt Thompson <matt@iastate.edu>
2016-10-31 15:54:33 -0500

    only return events that are in the future. also, limit response to next 4 events.

--- GA02/ExternalDataModules/sports.py
+++ GA02/ExternalDataModules/sports.py
@@ -1,106 +1,120 @@
 '''Pull sports data from Cyclones.com'''
-import requests
-import re
+import calendar
 import feedparser
-import time
-from subprocess import check_output
-
-uptime = check_output(['uptime'])
-print("\n")
-print('-------------------------------------------------------------')
-print(uptime.strip())
-print('-------------------------------------------------------------')
-print("\n")
+import datetime
 
 d = feedparser.parse('http://www.cyclones.com/calendar.ashx/calendar.rss?')
 
 
+def dateToTimeStamp(dt):
+    """convert a datetime obj to UNIX timestamp"""
+    response = calendar.timegm(dt.utctimetuple())
+    return response
+
+
 def feedparserEntry(parser):
-    print("\n" + time.strftime("%a, %b %d %I:%M %p"))
-    print("-----------------------------------------\n")
+    strr = ""
+    now = datetime.datetime.now()
+    nowStamp = dateToTimeStamp(now)
+    countr = 0
     for post in parser.entries:
-        print(post.title + "\n")
+        format = '%Y-%m-%d'
+        str = post.s_localstartdate[:10]
+        newDate = datetime.datetime.strptime(str, format)
+        stamp2 = dateToTimeStamp(newDate)
+        if stamp2 >= nowStamp and countr < 4:
+            countr = countr + 1
+            strr = strr + post.title + "\n"
+    if countr == 0:
+        strr = "No upcoming events for that sport are scheduled at this time."
+
+    return strr
+
+
+def responseAllSports():
+    d = feedparser.parse('http://www.cyclones.com/calendar.ashx/calendar.rss')
+    allsports = feedparserEntry(d)
+    return allsports
 
 
 def responseBasketballM():
     d = feedparser.parse('http://www.cyclones.com/calendar.ashx/calendar.rss?sport_id=4')
     menBasketballSchedule = feedparserEntry(d)
-    print(menBasketballSchedule)
+    return menBasketballSchedule
 
 
 def responseBasketballW():
     d = feedparser.parse('http://www.cyclones.com/calendar.ashx/calendar.rss?sport_id=8')
     womenBasketballSchedule = feedparserEntry(d)
-    print(womenBasketballSchedule)
+    return womenBasketballSchedule
 
 
 def responseCrossCountry():
     d = feedparser.parse('http://www.cyclones.com/calendar.ashx/calendar.rss?sport_id=5')
     crossCountry = feedparserEntry(d)
-    print(crossCountry)
+    return crossCountry
 
 
 def responseGolfM():
     d = feedparser.parse('http://www.cyclones.com/calendar.ashx/calendar.rss?sport_id=3')
     womenGolf = feedparserEntry(d)
-    print(womenGolf)
+    return womenGolf
 
 def responseGolfW():
     d = feedparser.parse('http://www.cyclones.com/calendar.ashx/calendar.rss?sport_id=10')
     menGolf = feedparserEntry(d)
-    print(menGolf)
+    return menGolf
 
 
 def responseFootball():
     d = feedparser.parse('http://www.cyclones.com/calendar.ashx/calendar.rss?sport_id=1')
     football = feedparserEntry(d)
-    print(football)
+    return football
 
 
 def responseWrestling():
     d = feedparser.parse('http://www.cyclones.com/calendar.ashx/calendar.rss?sport_id=17')
     wrestling = feedparserEntry(d)
-    print(wrestling)
+    return wrestling
 
 
 def responseTrackField():
     d = feedparser.parse('http://www.cyclones.com/calendar.ashx/calendar.rss?sport_id=6')
     trackField = feedparserEntry(d)
-    print(trackField)
+    return trackField
 
 
 def responseSoftball():
     d = feedparser.parse('http://www.cyclones.com/calendar.ashx/calendar.rss?sport_id=7')
     softball = feedparserEntry(d)
-    print(softball)
+    return softball
 
 
 def responseGymnastics():
     d = feedparser.parse('http://www.cyclones.com/calendar.ashx/calendar.rss?sport_id=11')
     gymnastics = feedparserEntry(d)
-    print(gymnastics)
+    return gymnastics
 
 
 def responseSoccer():
     d = feedparser.parse('http://www.cyclones.com/calendar.ashx/calendar.rss?sport_id=12')
     soccer = feedparserEntry(d)
-    print(soccer)
+    return soccer
 
 
 def responseSwmming():
     d = feedparser.parse('http://www.cyclones.com/calendar.ashx/calendar.rss?sport_id=13')
     swimming = feedparserEntry(d)
-    print(swimming)
+    return swimming
 
 
 def responseTennis():
     d = feedparser.parse('http://www.cyclones.com/calendar.ashx/calendar.rss?sport_id=14')
     tennis = feedparserEntry(d)
-    print(tennis)
+    return tennis
 
 
 def responseVolleyball():
     d = feedparser.parse('http://www.cyclones.com/calendar.ashx/calendar.rss?sport_id=16')
     volleyball = feedparserEntry(d)
-    print(volleyball)
-feedparserEntry(d)
+    return volleyball
