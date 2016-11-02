8a7d5a116a945fa0fde7d39cdb73f6eb66ab0196
wvoong <wvoong@iastate.edu>
2016-11-02 11:09:24 -0500

    added comments explaining code

--- GA02/ExternalDataModules/bus.py
+++ GA02/ExternalDataModules/bus.py
@@ -1,11 +1,13 @@
 import requests
 import xml.etree.ElementTree as ET
 
+#next bus api requires agency
+#this would be public transport agencies
 agency = 'cyride'
 
 _nextBusXMLAPIKey = 22
 
-
+#creates request and generates XML document of bus routes
 def getRouteTimes(stop):
     r = requests.get(
         'http://webservices.nextbus.com/service/publicXMLFeed?'
@@ -16,12 +18,14 @@ def getRouteTimes(stop):
         )
     answers = []
     tree = ET.fromstring(r.content)
-
+    #this begins parsing and extracting data from the XML
     for child in tree:
         if child.tag != "Error":
             routeName = child.attrib["routeTitle"]
             routeDescrip = ""
             predictions = []
+            #grabs data from XML and assigns them
+            #to appropriate time intervals and routes
             for child2 in child:
                 routeDescrip = child2.attrib["title"]
                 for child3 in child2:
