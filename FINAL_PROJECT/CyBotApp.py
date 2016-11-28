4231959e7f45219ff9c613d9257d96385bfeb839
Voong <wvoong@iastate.edu>
2016-11-28 11:52:18 -0600

    Added heading to UI and bus stop entry box

--- FINAL_PROJECT/CyBotApp.py
+++ FINAL_PROJECT/CyBotApp.py
@@ -2,11 +2,18 @@ from tkinter import *
 from tkinter.ttk import *
 
 window = Tk()
+
+team = Label(window, text="Ultimate")
+team.grid(row = 0, column = 0, columnspan = 2)
+
 weatherbtn = Button(window, text="Weather", width=12)
 weatherbtn.grid(row = 1, column = 0)
 
 busbtn = Button(window, text="Bus", width=12)
 busbtn.grid(row = 1, column = 1)
+bus_stop = StringVar()
+busentry = Entry(window, textvariable = bus_stop, width = 4)
+busentry.grid(row = 1, column = 2)
 
 sportbtn = Button(window, text="Sports", width=12)
 sportbtn.grid(row = 2, column = 0)
