18a69de71eb6333aa9bbc7ecf05103757d3d468a
Voong <wvoong@iastate.edu>
2016-11-28 11:43:44 -0600

    Created UI with button placement for each module

--- FINAL_PROJECT/CyBotApp.py
+++ FINAL_PROJECT/CyBotApp.py
@@ -1,6 +1,18 @@
 from tkinter import *
 from tkinter.ttk import *
 
-root = Tk()
-btn = ttk.Button(text="Weather")
-btn.pack()
+window = Tk()
+weatherbtn = Button(window, text="Weather", width=12)
+weatherbtn.grid(row = 1, column = 0)
+
+busbtn = Button(window, text="Bus", width=12)
+busbtn.grid(row = 1, column = 1)
+
+sportbtn = Button(window, text="Sports", width=12)
+sportbtn.grid(row = 2, column = 0)
+
+newsbtn = Button(window, text="News", width=12)
+newsbtn.grid(row = 2, column = 1)
+
+window.mainloop()
+#if pressed, update module by requesting new data
