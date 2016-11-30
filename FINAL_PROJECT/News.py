0a619ba7c2e2a974aa46e719157adaa4b20cc59d
ndburger <ndburger@iastate.edu>
2016-11-30 14:31:36 -0600

    Got news to print headlines and links, dont know how to add to kinter

--- FINAL_PROJECT/News.py
+++ FINAL_PROJECT/News.py
@@ -1,10 +1,42 @@
-'''Pull sports data from the Ames Trubune'''
-import calendar
+'''Pull sports data from the Iowa State'''
 import feedparser
-import datetime
 
+# Function to fetch the rss feed and return the parsed RSS
+def parseRSS( rss_url ):
+    return feedparser.parse( rss_url )
 
-feed = "http://www.public.iastate.edu/~nscentral/news/rss.xml"
-d = feedparser.parse(feed)
-print (d['feed']['title'])
-print (d['feed']['link'])
+# Function grabs the rss feed headlines (titles) and returns them as a list
+def getHeadlines( rss_url ):
+    headlines = []
+
+    feed = parseRSS( rss_url )
+    for newsitem in feed['items']:
+        headlines.append(newsitem['title'])
+
+    return headlines
+# Funtion to add links to headlines
+def getLinks( rss_url ):
+    links = []
+
+    feed = parseRSS( rss_url )
+    for newsitem in feed['items']:
+        links.append(newsitem['link'])
+    return links
+# A list to hold all headlines
+allheadlines = []
+alllinks = []
+# List of RSS feeds that we will fetch and combine
+newsurls = {
+    'ISU':           'http://www.public.iastate.edu/~nscentral/news/rss.xml',
+}
+# Iterate over the feed urls
+for key,url in newsurls.items():
+    # Call getHeadlines() and combine the returned headlines with allheadlines
+    allheadlines.extend( getHeadlines( url ) )
+    alllinks.extend( getLinks( url ))
+
+# Iterate over the allheadlines list and print each headline
+for hl in allheadlines:
+    print(hl)
+for l in alllinks:
+    print(l)
