3203ba6f34f2e6cb80d33168e9e84ddbf5dc25e2
ndburger <ndburger@iastate.edu>
2016-11-30 11:34:56 -0600

    Unsucessfully implemented news

--- FINAL_PROJECT/News.py
+++ FINAL_PROJECT/News.py
@@ -4,26 +4,7 @@ import feedparser
 import datetime
 
 
-feedURL = "http://www.public.iastate.edu/~nscentral/news/rss.xml"
-feed = feedparser.parse(feedURL)
-
-
-def feedparserEntry(parser):
-    strr = ""
-    now = datetime.datetime.now()
-    nowStamp = dateToTimeStamp(now)
-    countr = 0
-    for post in parser.entries:
-        format = '%Y-%m-%d'
-        str = post.s_localstartdate[:10]
-        newDate = datetime.datetime.strptime(str, format)
-        stamp2 = dateToTimeStamp(newDate)
-        if stamp2 >= nowStamp and countr < 4:
-            countr = countr + 1
-            strr = strr + post.title + " in " + post.ev_location + "\n"
-    if countr == 0:
-        strr = "No headlines are available at this time."
-
-    return strr
-
-def getHeadline(title):
+feed = "http://www.public.iastate.edu/~nscentral/news/rss.xml"
+d = feedparser.parse(feed)
+print (d['feed']['title'])
+print (d['feed']['link'])
